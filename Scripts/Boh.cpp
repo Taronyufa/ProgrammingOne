b51bddc33b450c4f1ea91fa251e39b16af308743
Taronyufa <emanuele.stefani.geo@gmail.com>
2023-09-19 11:58:04 +0200

    just exercises

--- Scripts/Boh.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-
-int main(string args[])
-{
-    cout<<"Hello world!";
-    return 0;
-}
---
+++ Scripts/SoluzioniEqSecondoGrado/SoluzioniEqSecondoGrado.cpp
@@ -0,0 +1,32 @@
+using namespace std;
+#include <iostream>
+#include <cmath>
+
+void getNumbers(int &a, int &b, int &c);
+
+int main(){
+
+    int a, b, c;
+    getNumbers(a, b, c);
+    
+    // find the two solutions
+    double delta = sqrt((b * b) - 4 * (a * c));
+    double solution1 = (-b + delta) / (2 * a);
+    double solution2 = (-b - delta) / (2 * a);
+
+    cout << "solution 1 : " << solution1 << endl << "solution 2 : " << solution2;
+
+    return 0;
+}
+
+// gets a, b and c 
+void getNumbers(int &a, int &b, int &c){
+    
+    cout << "inserire a: ";
+    cin >> a;
+    cout << "inserire b: ";
+    cin >> b;
+    cout << "inserire c: ";
+    cin >> c;
+
+}
