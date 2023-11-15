7b80d8e0dd2dde85a10ee798de1d442139b7612b
Taronyufa <emanuele.stefani.geo@gmail.com>
2023-11-15 21:18:33 +0100

    there's only one exercise left

--- Scripts/Es 58/Punteggio.cpp
+++ Scripts/Es 58/Punteggio.cpp
@@ -10,3 +10,33 @@
 // desidera, gli chieda di immettere da tastiera una stringa contenente al
 // massimo 31 caratteri, chiami la funzione di cui sopra e, in caso di dato valido,
 // ne stampi a video il valore di ritorno.
+
+using namespace std;
+#include <iostream>
+#include <cmath>
+
+int main(){
+    string s;
+
+    cout << "Inserisci una Stringa: ";
+    cin >> s;
+
+    for(int i = 0; s[i] != '\0'; i++){
+        if (s[i] >= 'A' && s[i] <= 'Z'){
+            s[i] += 32;
+        }
+        if(!(s[i] >= 'a' && s[i] <= 'z')){
+            return -1;
+        }
+    }
+
+    int sum = 0;
+
+    for(int i = 0; s[i+1] != '\0'; i++){
+        sum += abs(int(s[i] - s[i+1]));
+    }
+
+    cout << sum;
+
+    return 0;
+}
